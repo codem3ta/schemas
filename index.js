9a8d31728b52c0bfe680c346bf93b045090998e5
Moritz Lübken <moritz.luebken@codemeta.de>
2023-04-22 13:34:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  asset: require('./asset/asset.json'),
+  pc: require('./asset/pc/pc.json'),
+  project: require('./project/project.json'),
+  party: require('./party/party.json'),
+  secret: require('./secret/secret.json')
+}
