16abb6cd9c2907c6f4fbbd82a47d89e84c7ded54
Moritz Lübken <moritz.luebken@codemeta.de>
2023-05-22 15:54:45 +0200

    New schemas

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 module.exports = {
   asset: require('./asset/asset.json'),
-  pc: require('./asset/pc/pc.json'),
-  project: require('./project/project.json'),
+  pc: require('./asset/pc.json'),
+  car: require('./asset/car.json'),
+  project: require('./project.json'),
   party: require('./party/party.json'),
-  secret: require('./secret/secret.json')
+  secret: require('./secret.json'),
+  task: require('./task.json'),
+  activity: require('./activity/activity.json'),
+  timeRecord: require('./activity/timeRecord.json'),
+  travelRecord: require('./activity/travelRecord.json'),
 }
