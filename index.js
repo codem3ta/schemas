5f1d7d417ce63b8f03faf28ff02e754730e4a5d5
Moritz Lübken <moritz.luebken@codemeta.de>
2023-06-22 09:59:47 +0200

    Restructure and add schemas

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ module.exports = {
   timeRecord: require('./activity/timeRecord.json'),
   travelRecord: require('./activity/travelRecord.json'),
   shipment: require('./shipment.json'),
+  instance: require('./instance.json'),
 }
