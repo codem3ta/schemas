a78a74add75fc5524e0d30c4da5b6cf10b8438ba
Moritz Lübken <moritz.luebken@codemeta.de>
2024-03-07 15:24:48 +0100

    Add bucket schema to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   asset: require('./asset/asset.json'),
+  bucket: require('./asset/bucket.json'),
   pc: require('./asset/pc.json'),
   car: require('./asset/car.json'),
   project: require('./project.json'),
